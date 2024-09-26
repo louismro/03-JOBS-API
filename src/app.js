4920e2a43410a48db17c137751ebf48d5e135733
Thomas ROBERT <thomas_3004@hotmail.fr>
2024-09-26 16:05:43 +0200

    feat: Add register and login user

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
+import 'express-async-errors';
 import express from 'express';
 const app = express();
 import errorHandler from './middlewares/error-handler.js';
 import notFound from './middlewares/not-found.middleware.js';
+import connectDB from './config/db.config.js';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+connectDB();
+
+app.use(express.json());
+
+import { auth } from './features/auth/index.js';
+
+app.use('/api/v1/auth', auth);
 
 app.use(notFound);
 app.use(errorHandler);
--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -3,9 +3,19 @@ import { StatusCodes } from 'http-status-codes';
 const errorHandler = (err, _req, res, _next) => {
   console.error(err);
 
-  res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ msg: err.message });
+  const msg =
+    err.message ||
+    "Une erreur s'est produite, veuillez réessayer plus tard";
+  const statusCode =
+    err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+
+  if (err.code === 11000) {
+    return res
+      .status(StatusCodes.CONFLICT)
+      .json({ msg: "L'adresse email existe déjà" });
+  }
+
+  res.status(statusCode).json({ msg });
 };
 
 export default errorHandler;
